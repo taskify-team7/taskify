7d045da8951072b8faf38321acdfdecf081b2421
vinoankr <vinoankr@gmail.com>
2024-04-14 14:22:19 +0900

    feat: context AuthProvider 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import "./Global.css";
 import Router from "./Router";
+import AuthProvider from "./contexts/AuthProvider";
 
 function App() {
-  return <Router />;
+  return (
+    <AuthProvider>
+      <Router />
+    </AuthProvider>
+  );
 }
 
 export default App;
