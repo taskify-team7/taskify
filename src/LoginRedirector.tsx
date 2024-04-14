42ca9c5b2b1c451391a229675cb7422584273521
vinoankr <vinoankr@gmail.com>
2024-04-14 14:25:06 +0900

    feat: 로그인이 필요한 페이지들은 라우터에서 분기, axios interceptor로 401응답(권한 없음)이 오거나 로컬스토리지에 유저 정보가 없을 경우에는  로그인 페이지로 리다이렉트되도록 설정

---
+++ src/LoginRedirector.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import client from "./api/axios";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "./contexts/AuthProvider";
+
+export default function LoginRedirector({ children }: { children: any }) {
+  const navigate = useNavigate();
+  const { contextLogout } = useAuth();
+
+  useEffect(() => {
+    const requestInterceptor = client.interceptors.request.use(function (
+      config
+    ) {
+      const token = localStorage.getItem("token");
+      config.headers.Authorization = token ? `Bearer ${token}` : "";
+      return config;
+    });
+    const responseInterceptor = client.interceptors.response.use(
+      function (response: any) {
+        return response;
+      },
+      function (error: any) {
+        const status = error.response?.status;
+        if (status === 401) {
+          contextLogout();
+          console.log(
+            "server responded with 401 status. redirecting to login page."
+          );
+          navigate("/login");
+        }
+        return Promise.reject(error);
+      }
+    );
+    return () => {
+      client.interceptors.response.eject(requestInterceptor);
+      client.interceptors.response.eject(responseInterceptor);
+    };
+  }, [contextLogout, navigate]);
+
+  return <>{children}</>;
+}
