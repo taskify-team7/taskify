ce3ed99564fd48107c471a90e65f763f23654c77
78-artilleryman <ywy040150@gmail.com>
2024-04-18 19:11:53 +0900

    feat: image 선택 input 기능 구현

--- src/components/Input/ImageInput.tsx
+++ src/components/Input/ImageInput.tsx
@@ -1,12 +1,25 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import styles from "./ImageInput.module.css";
 import { CommonInputType } from "../../interface/Input";
 
 function ImageInput({ label, inputOnChange, value }: CommonInputType) {
   const imageInput = useRef<any>();
+  const [pickedImage, setPickedImage] = useState<any>();
 
   const handleImageInput = () => {
     imageInput.current.click();
+  };
+
+  const handleImageChange = (e: any) => {
+    const file = e.target.files[0];
+    if (!file) {
+      return;
+    }
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
   };
 
   return (
@@ -14,22 +27,39 @@ function ImageInput({ label, inputOnChange, value }: CommonInputType) {
       <label htmlFor="iamge" className={styles.content_label}>
         {label}
       </label>
-      <input
-        id="iamge"
-        type="file"
-        accept="image/png, image/jpeg"
-        className={styles.content_input}
-        value={value}
-        onChange={inputOnChange}
-        ref={imageInput}
-      />
-      <button
-        type="button"
-        className={styles.content_image}
-        onClick={handleImageInput}
-      >
-        <img src="/Icons/add_icon.svg" alt="add_Image" />
-      </button>
+      <div className={styles.content_image}>
+        <input
+          id="iamge"
+          type="file"
+          accept="image/png, image/jpeg"
+          className={styles.content_image_input}
+          value={value}
+          onChange={handleImageChange}
+          ref={imageInput}
+        />
+        {pickedImage ? (
+          <>
+            <div className={styles.imageCover} onClick={handleImageInput}>
+              <img src="/Icons/edit_pen.svg" alt="edit" />
+            </div>
+            <img
+              className={styles.pickedImage}
+              src={pickedImage}
+              alt="add_Image"
+            />
+          </>
+        ) : (
+          <>
+            <button
+              type="button"
+              className={styles.content_image_button}
+              onClick={handleImageInput}
+            >
+              <img src="/Icons/add_icon.svg" alt="add_Image" />
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
 }
