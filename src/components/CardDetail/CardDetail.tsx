23cf7f200f8dd989ae464de6d0e3489c00d363a8
78-artilleryman <ywy040150@gmail.com>
2024-04-20 14:48:59 +0900

    feat: card detal 컴포넌트 card data에 맞게 동적으로 바뀌게 구현

--- src/components/CardDetail/CardDetail.tsx
+++ src/components/CardDetail/CardDetail.tsx
@@ -12,13 +12,11 @@ interface CardDetailProps {
 }
 
 function CardDetail({ handleModalClose, card }: CardDetailProps) {
-  console.log(card);
-
   return (
     <ModalContainer handleModalClose={handleModalClose}>
       <div className={styles.cardDetail}>
         <div className={styles.cardDetail_header}>
-          <h2>새로운 일정 관리</h2>
+          <h2>{card.title}</h2>
           <div className={styles.cardDetail_header_option}>
             <img src="/Icons/kebab.svg" alt="menu" />
             <img
@@ -32,33 +30,35 @@ function CardDetail({ handleModalClose, card }: CardDetailProps) {
           <div>
             <label className={styles.cardDetail_mobile_label}>담당자</label>
             <div className={styles.cardDetail_mobile_user}>
-              <div className={styles.cardDetail_mobile_profile}>C</div>
-              <p>윤병현</p>
+              <div className={styles.cardDetail_mobile_profile}>
+                {card.assignee.profileImageUrl
+                  ? card.assignee.profileImageUrl
+                  : card.assignee.nickname[0]}
+              </div>
+              <p>{card.assignee.nickname}</p>
             </div>
           </div>
           <div>
             <label className={styles.cardDetail_mobile_label}>마감일</label>
-            <p className={styles.cardDetail_mibile_date}>2022.12.30 19:00</p>
+            <p className={styles.cardDetail_mibile_date}>{card.dueDate}</p>
           </div>
         </div>
         <div className={styles.cardDetail_main}>
           <div className={styles.cardDetail_content}>
             <div className={styles.cardDetail_labels}>
+              {/**이부분 컬럼 이름도 변경되게 해야함*/}
               <div className={styles.cardDetail_columnName}>To do</div>
               <div className={styles.cardDetail_tags}>
-                <Tag TagName="test" />
+                {card?.tags.map((tag, i) => (
+                  <Tag key={i} TagName={tag} />
+                ))}
               </div>
             </div>
             <div className={styles.cardDetail_text}>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Vestibulum finibus nibh arcu, quis consequat ante cursus eget.
-                Cras mattis, nulla non laoreet porttitor, diam justo laoreet
-                eros, vel aliquet diam elit at leo.
-              </p>
+              <p>{card.description}</p>
             </div>
             <div className={styles.cardDetail_img}>
-              <img src="/Image/todoImg.svg" alt="content_image" />
+              <img src={card.imageUrl} alt="content_image" />
             </div>
             <form>
               <ComentInput />
@@ -71,13 +71,17 @@ function CardDetail({ handleModalClose, card }: CardDetailProps) {
             <div>
               <label className={styles.cardDetail_sidebar_label}>담당자</label>
               <div className={styles.cardDetail_sidebar_user}>
-                <div className={styles.cardDetail_coment_profile}>C</div>
-                <p>윤병현</p>
+                <div className={styles.cardDetail_coment_profile}>
+                  {card.assignee.profileImageUrl
+                    ? card.assignee.profileImageUrl
+                    : card.assignee.nickname[0]}
+                </div>
+                <p>{card.assignee.nickname}</p>
               </div>
             </div>
             <div>
               <label className={styles.cardDetail_sidebar_label}>마감일</label>
-              <p className={styles.cardDetail_sidebar_date}>2022.12.30 19:00</p>
+              <p className={styles.cardDetail_sidebar_date}>{card.dueDate}</p>
             </div>
           </div>
         </div>
