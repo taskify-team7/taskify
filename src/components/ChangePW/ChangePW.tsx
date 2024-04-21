d073eaa0828c92ae52e383980d24e9040221e07b
yeeZinu <doglife222@gmail.com>
2024-04-22 03:33:24 +0900

    feat(#18): 현재비밀번호 모달 구현

--- src/components/ChangePW/ChangePW.tsx
+++ src/components/ChangePW/ChangePW.tsx
@@ -1,8 +1,10 @@
-import React, { FormEvent, useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import CommonInput from '../Input/CommonInput';
 import BaseButton from '../BaseButton/BaseButton';
 import style from './ChangePW.module.css';
 import { useForm } from 'react-hook-form';
+import { logIn } from '../../api/auth';
+import PasswordErrorModal from '../Modal/PasswordErrorModal';
 
 interface FormValues {
   password: string;
@@ -26,11 +28,34 @@ export default function ChangePW() {
       newPasswordConfirmation: '',
     },
   });
-
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const newPassword = watch('newPassword');
   const newPasswordConfirmation = watch('newPasswordConfirmation');
 
-  const onSubmit = (data: FormValues) => {
+  let userEmail = '';
+  const userString = localStorage.getItem('user');
+  if (userString) {
+    const userObject = JSON.parse(userString);
+    userEmail = userObject.email;
+  }
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
+  const onSubmit = async (data: FormValues) => {
+    const LoginData = {
+      email: userEmail,
+      password: data.password,
+    };
+
+    try {
+      await logIn(LoginData);
+    } catch (error) {
+      console.log(error.response.data.message);
+      setIsModalOpen(true);
+    }
+
     if (newPassword !== newPasswordConfirmation) {
       setError('newPasswordConfirmation', {
         type: 'manual',
@@ -47,43 +72,48 @@ export default function ChangePW() {
   };
 
   return (
-    <div className={style.containor}>
-      <h2 className={style.mainText}>비밀번호 변경</h2>
-      <form className={style.formContainor} onSubmit={handleSubmit(onSubmit)}>
-        <CommonInput
-          label="비밀번호"
-          placeholder="현재 비밀번호 입력"
-          type="password"
-          name="password"
-          validation={register('password', {
-            required: '비밀번호는 필수 입력 사항입니다.',
-          })}
-          errors={errors}
-        />
-        <CommonInput
-          label="새 비밀번호"
-          placeholder="새 비밀번호 입력"
-          type="password"
-          name="newPassword"
-          validation={register('newPassword', {
-            required: '비밀번호는 필수 입력 사항입니다.',
-          })}
-          errors={errors}
-        />
-        <CommonInput
-          label="새 비밀번호 확인"
-          placeholder="새 비밀번호 입력"
-          type="password"
-          name="newPasswordConfirmation"
-          validation={register('newPasswordConfirmation', {
-            required: '비밀번호는 필수 입력 사항입니다.',
-          })}
-          errors={errors}
-        />
-        <div className={style.buttonContainor}>
-          <BaseButton text="변경" styleType="accept" type="submit" />
-        </div>
-      </form>
-    </div>
+    <>
+      {isModalOpen && (
+        <PasswordErrorModal handleModalClose={handleModalClose} />
+      )}
+      <div className={style.containor}>
+        <h2 className={style.mainText}>비밀번호 변경</h2>
+        <form className={style.formContainor} onSubmit={handleSubmit(onSubmit)}>
+          <CommonInput
+            label="비밀번호"
+            placeholder="현재 비밀번호 입력"
+            type="password"
+            name="password"
+            validation={register('password', {
+              required: '비밀번호는 필수 입력 사항입니다.',
+            })}
+            errors={errors}
+          />
+          <CommonInput
+            label="새 비밀번호"
+            placeholder="새 비밀번호 입력"
+            type="password"
+            name="newPassword"
+            validation={register('newPassword', {
+              required: '비밀번호는 필수 입력 사항입니다.',
+            })}
+            errors={errors}
+          />
+          <CommonInput
+            label="새 비밀번호 확인"
+            placeholder="새 비밀번호 입력"
+            type="password"
+            name="newPasswordConfirmation"
+            validation={register('newPasswordConfirmation', {
+              required: '비밀번호는 필수 입력 사항입니다.',
+            })}
+            errors={errors}
+          />
+          <div className={style.buttonContainor}>
+            <BaseButton text="변경" styleType="accept" type="submit" />
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
