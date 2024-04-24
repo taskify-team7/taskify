e68da96eb3c19e6d4f8be7c4a6daa3fbeff9cad8
78-artilleryman <ywy040150@gmail.com>
2024-04-24 14:44:53 +0900

    feat: 대시보드, 컬럼 API 통신 코드에 toast UI 연결

--- src/api/dashboard.ts
+++ src/api/dashboard.ts
@@ -1,4 +1,5 @@
 import client from "./axios";
+import { toast } from "react-toastify";
 
 interface ApiPrameterType {
   navigationMethod: string;
@@ -8,86 +9,156 @@ interface ApiPrameterType {
 }
 
 export const getDashboardList = async (params: ApiPrameterType) => {
-  const { data } = await client.get("dashboards", {
-    params: {
-      navigationMethod: params.navigationMethod,
-      cursorId: params.cursorId,
-      page: params.page,
-      size: params.size,
-    },
-  });
+  try {
+    const { data } = await client.get("dashboards", {
+      params: {
+        navigationMethod: params.navigationMethod,
+        cursorId: params.cursorId,
+        page: params.page,
+        size: params.size,
+      },
+    });
 
-  return data;
+    return data;
+  } catch (e: any) {
+    console.log(e);
+  }
 };
 
 export const getInviteList = async () => {
-  const { data } = await client.get("invitations", {
-    params: {
-      size: 6,
-    },
-  });
-  return data;
+  try {
+    const { data } = await client.get("invitations", {
+      params: {
+        size: 6,
+      },
+    });
+    return data;
+  } catch (e: any) {
+    console.log(e);
+  }
 };
 
 export const updateInvitations = async (
   invitationId: number,
   inviteAccepted: boolean
 ) => {
-  const { data } = await client.put(`invitations/${invitationId}`, {
-    inviteAccepted: inviteAccepted,
-  });
-  return data;
+  try {
+    const { data } = await client.put(`invitations/${invitationId}`, {
+      inviteAccepted: inviteAccepted,
+    });
+    return data;
+  } catch (e: any) {
+    console.log(e);
+    toast.error(e.response.data.message);
+  }
 };
 
 export const createDashboard = async (title: string, color: string) => {
-  const { data } = await client.post("dashboards", {
-    title: title,
-    color: color,
-  });
-  return data;
+  try {
+    const result = await client.post("dashboards", {
+      title: title,
+      color: color,
+    });
+    if (result.status === 201) {
+      toast.success("새로운 대시보드가 생성되었습니다.");
+      return result.data;
+    }
+  } catch (e: any) {
+    console.log(e);
+    toast.error(e.response.data.message);
+  }
 };
 
 export const getDashboard = async (id: string) => {
-  const { data } = await client.get(`dashboards/${id}`);
-  return data;
+  try {
+    const { data } = await client.get(`dashboards/${id}`);
+    return data;
+  } catch (e: any) {
+    console.log(e);
+  }
 };
 
 export const createColumn = async (title: string, dashboardId: number) => {
-  const { data } = await client.post(`columns`, {
-    title: title,
-    dashboardId: dashboardId,
-  });
+  try {
+    const result = await client.post(`columns`, {
+      title: title,
+      dashboardId: dashboardId,
+    });
 
-  return data;
+    if (result.status === 201) {
+      toast.success("새로운 컬럼이 생성되었습니다.");
+      return result.data;
+    }
+  } catch (e: any) {
+    console.log(e);
+    toast.error(e.response.data.message);
+  }
 };
 
 export const getColumns = async (id: string) => {
-  const { data } = await client.get("columns", { params: { dashboardId: id } });
-  return data.data;
+  try {
+    const { data } = await client.get("columns", {
+      params: { dashboardId: id },
+    });
+    return data.data;
+  } catch (e: any) {
+    console.log(e);
+  }
 };
 
 export const deleteColumn = async (columnId: number) => {
-  const { data } = await client.delete(`columns/${columnId}`);
-  return data;
+  try {
+    const { data } = await client.delete(`columns/${columnId}`);
+
+    return data;
+  } catch (e: any) {
+    console.log(e);
+    toast.error(e.response.data.message);
+  }
 };
 
 export const updateColumn = async (columnId: number, newTitle: string) => {
-  console.log(newTitle);
-  const { data } = await client.put(`columns/${columnId}`, { title: newTitle });
-  return data;
+  try {
+    const result = await client.put(`columns/${columnId}`, {
+      title: newTitle,
+    });
+    if (result.status === 200) {
+      toast.success("컬럼이 수정되었습니다.");
+    }
+
+    return result.data;
+  } catch (e: any) {
+    console.log(e);
+    toast.error(e.response.data.message);
+  }
 };
 
 export const getMembers = async (id: string) => {
-  const { data } = await client.get("members", { params: { dashboardId: id } });
-  return data.members;
+  try {
+    const { data } = await client.get("members", {
+      params: { dashboardId: id },
+    });
+    return data.members;
+  } catch (e: any) {
+    console.log(e);
+  }
 };
 
 export const dashboardInvite = async (email: string, dashboardId: number) => {
-  const { data } = await client.post(`dashboards/${dashboardId}/invitations`, {
-    email: email,
-  });
+  try {
+    const result = await client.post(`dashboards/${dashboardId}/invitations`, {
+      email: email,
+    });
 
-  return data;
+    if (result.status === 201) {
+      toast.success("초대가 완료되었습니다.");
+    }
+
+    return result.data;
+  } catch (e: any) {
+    console.log(e);
+    toast.error(e.response.data.message);
+  }
 };
 
 export const changeColumnImageURL = async (
