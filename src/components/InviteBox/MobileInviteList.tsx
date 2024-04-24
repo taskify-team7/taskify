8506543fab504469b814e30c3dcaf97f6581b73a
78-artilleryman <ywy040150@gmail.com>
2024-04-24 10:46:37 +0900

    feat: 모바일 사이즈에서 대시보드 초대 수락, 거절할 수 있게 기능 추가

--- src/components/InviteBox/MobileInviteList.tsx
+++ src/components/InviteBox/MobileInviteList.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import styles from "./MobileInviteList.module.css";
 import { InviteDataType } from "../../interface/DashboardType";
+import { updateInvitations } from "../../api/dashboard";
 
 function MobileInviteList({ inviteData }: { inviteData: InviteDataType[] }) {
+  const invitationsHandle = async (
+    invitationId: number,
+    inviteAccepted: boolean
+  ) => {
+    const res = await updateInvitations(invitationId, inviteAccepted);
+  };
   return (
     <div className={styles.inviteList}>
       {inviteData.map((invite) => (
@@ -16,8 +23,18 @@ function MobileInviteList({ inviteData }: { inviteData: InviteDataType[] }) {
             <p>{invite.inviter.nickname}</p>
           </div>
           <div className={styles.inviteItem_buttons}>
-            <button className={styles.a_button}>수락</button>
-            <button className={styles.c_button}> 거절</button>
+            <button
+              className={styles.a_button}
+              onClick={() => invitationsHandle(invite.id, true)}
+            >
+              수락
+            </button>
+            <button
+              className={styles.c_button}
+              onClick={() => invitationsHandle(invite.id, false)}
+            >
+              거절
+            </button>
           </div>
         </div>
       ))}
