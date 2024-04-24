5476f903a5c368e2150868dbb75c7c22ec5583e6
ryugwanghyoen <rgh4422@google.com>
2024-04-24 12:31:01 +0900

    vercel error

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from "./LandingPage.module.css";
 import LandingNavbar from "../../components/LandingNavbar/LandingNavbar";
 import Footer from "../../components/LandingFooter/Footer";
