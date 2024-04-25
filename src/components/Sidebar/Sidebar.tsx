eb715d60360dcb25f533bbaededa342f46bc2852
yeeZinu <doglife222@gmail.com>
2024-04-25 20:03:16 +0900

    feat: 사이드바 페이지네이션 구현

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,21 +1,29 @@
-import { ReactNode } from "react";
-import styles from "./Sidebar.module.css";
-import LogoImage from "../../assets/mediumLogo.svg";
-import SmallLogoImage from "../../assets/smallLogo.svg";
-import PlusIcon from "../../assets/plus.svg";
-import Crown from "../../assets/crown.svg";
-import { useNavigate, useParams } from "react-router-dom";
-import { DashBoardType } from "../../interface/DashboardType";
-import { createPortal } from "react-dom";
-import DashBoardCreateModal from "../Modal/DashBoardCreateModal";
-import { useModal } from "../../hooks/useModal";
+import { ReactNode } from 'react';
+import styles from './Sidebar.module.css';
+import LogoImage from '../../assets/mediumLogo.svg';
+import SmallLogoImage from '../../assets/smallLogo.svg';
+import PlusIcon from '../../assets/plus.svg';
+import Crown from '../../assets/crown.svg';
+import { useNavigate, useParams } from 'react-router-dom';
+import { DashBoardType } from '../../interface/DashboardType';
+import { createPortal } from 'react-dom';
+import DashBoardCreateModal from '../Modal/DashBoardCreateModal';
+import { useModal } from '../../hooks/useModal';
 
 export default function Sidebar({
   children,
   dashboards,
+  page,
+  nextPage,
+  prevPage,
+  totalPage,
 }: {
   children: ReactNode;
   dashboards: DashBoardType[] | null;
+  page: number;
+  nextPage: () => void;
+  prevPage: () => void;
+  totalPage?: number;
 }) {
   const navigate = useNavigate();
   const { id } = useParams();
@@ -35,13 +43,13 @@ export default function Sidebar({
             src={LogoImage}
             alt="logo"
             className={styles.logo}
-            onClick={() => navigate("/dashboard")}
+            onClick={() => navigate('/dashboard')}
           />
           <img
             src={SmallLogoImage}
             alt="logo"
             className={styles.smallLogo}
-            onClick={() => navigate("/dashboard")}
+            onClick={() => navigate('/dashboard')}
           />
           <div className={styles.head}>
             <span className={styles.headText}>Dash Boards</span>
@@ -58,7 +66,7 @@ export default function Sidebar({
                 key={dashboard.id}
                 onClick={() => navigate(`/dashboard/${dashboard.id}`)}
                 className={`${styles.dashboardListItem} + ${
-                  id && dashboard.id === Number(id) ? styles.selected : ""
+                  id && dashboard.id === Number(id) ? styles.selected : ''
                 }`}
               >
                 <div
@@ -72,6 +80,32 @@ export default function Sidebar({
               </li>
             ))}
           </ul>
+          <div className={styles.pagenationBtn}>
+            <button
+              className={styles.leftBtn}
+              type="button"
+              onClick={prevPage}
+              disabled={page === 1}
+            >
+              {page === 1 ? (
+                <img src="/Icons/pagination_prev.svg" alt="prev" />
+              ) : (
+                <img src="/Icons/leftArrow.svg" alt="prev" />
+              )}
+            </button>
+            <button
+              className={styles.rightBtn}
+              type="button"
+              onClick={nextPage}
+              disabled={page === totalPage}
+            >
+              {page === totalPage ? (
+                <img src="/Icons/pagination_next.svg" alt="next" />
+              ) : (
+                <img src="/Icons/rightArrow.svg" alt="next" />
+              )}
+            </button>
+          </div>
         </div>
         <div className={styles.contents}>{children}</div>
       </div>
