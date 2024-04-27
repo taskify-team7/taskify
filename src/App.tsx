4f828e4c8cf82d8aa21dcd631d62282678d80f97
78-artilleryman <ywy040150@gmail.com>
2024-04-28 00:16:20 +0900

    docs: ga 라이브러리를 ga4로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,9 @@ import Router from "./Router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AuthProvider from "./contexts/AuthProvider";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import ReactGA from "react-ga";
 
 function App() {
   const queryClient = new QueryClient();
-
-  const gaTrackingId: string | undefined = process.env.REACT_APP_GA_TRACKING_ID;
-  if (gaTrackingId) {
-    ReactGA.initialize(gaTrackingId, { debug: true });
-    ReactGA.pageview(window.location.pathname);
-  } else {
-    console.error(
-      "Google Analytics tracking ID is undefined. Make sure it is set in your environment variables."
-    );
-  }
 
   return (
     <div className="App">
--- src/DashBoardRedirector.tsx
+++ src/DashBoardRedirector.tsx
@@ -1,5 +1,6 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "./contexts/AuthProvider";
+import RouteChangeTracker from "./RouteChangeTracker";
 
 export default function DashBoardRedirector() {
   const { getUser } = useAuth();
@@ -8,6 +9,7 @@ export default function DashBoardRedirector() {
   }
   return (
     <>
+      <RouteChangeTracker />
       <Outlet />
     </>
   );
